846bee032cacbcb4846de68384a54224d97a011c
nathan hains <nathanhains14@yahoo.com>
2021-02-03 18:52:58 -0500

    commit tree 0f6ad93d70d327aa03282ecee5195efee51f2b18

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+def reverse_each_word(string)
+    string_array = string.split
+    new_array = []
+    string_array.collect do |word|
+        new_array << word.reverse
+    end 
+    new_array.join(" ")
+end
+
